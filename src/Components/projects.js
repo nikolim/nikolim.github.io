7876b66bfab057b11a11e53c3b980c6c79bf6e3a
Nikolai Limbrunner <nikolai.limbrunner@web.de>
2022-08-23 09:29:44 +0200

    Updated project descriptions

--- src/Components/projects.js
+++ src/Components/projects.js
@@ -58,6 +58,9 @@ const TailwindBadge = function () {
 const TorchBadge = function () {
     return (<span className={`bg-rose-100 text-rose-800 ${commonBadgeStyle}`}>PyTorch</span>);
 }
+const NodeBadge = function () {
+    return (<span className={`bg-green-100 text-green-800 ${commonBadgeStyle}`}>NodeJS</span>);
+}
 
 const Card = function ({name, type, img, description, badges}) {
     return (
@@ -71,8 +74,8 @@ const Card = function ({name, type, img, description, badges}) {
                         {type()}
                     </div>
                 </div>
-                <img className="rounded-lg w-full h-32 object-cover" src={img} alt=""/>
-                <p className="mb-3 font-normal text-gray-200">{description}</p>
+                <img className="my-2 rounded-lg w-full h-32 object-cover" src={img} alt=""/>
+                <p className="mb-3 font-normal text-gray-200" dangerouslySetInnerHTML={{__html: description}}/>
                 <div className="flex flex-wrap">
                     {badges.map(badge => badge())}
                 </div>
@@ -83,45 +86,45 @@ const Card = function ({name, type, img, description, badges}) {
 const Projects = function () {
     return (
         <div className="flex-col pt-9">
-            <div className="text-center text-white text-4xl">
+            <div className="text-center text-white text-4xl pb-4 font-medium">
                 Projects
             </div>
             <div
                 className="p-4 grid grid-cols-1 sm:grid-cols-1 sm:p-4 md:grid-cols-2 md:p-6 lg:grid-cols-3 lg:p-8 xl:grid-cols-3 xl:p-10 gap-8 m-auto max-w-6xl">
-                <Card name={"Cooperative Lange Management"}
+                <Card name={"CLM Lane Management"}
                       type={IndustryBadge}
                       img={clm}
-                      description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."}
+                  description={`Development of distributed backend services and onboard software to enable cooperative maneuvers like lane merging of connected vehicles. Levering 5G and Mobile-Edge-Computing to reach low latency <a href="https://www.youtube.com/watch?v=daxptHOXTI4" style="text-decoration: underline">(Promo video).</a>`}
                       badges={[PythonBadge, JavascriptBadge, GolangBadge, AMQPBadge, K8sBadge, InfluxBadge, GrafanaBadge]}
-                />
-                <Card name={"IOT Protocols"}
-                      type={IndustryBadge}
-                      img={iot}
-                      description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."}
-                      badges={[PythonBadge, GrpcBadge, MQTTBadge, AMQPBadge]}
                 />
                 <Card name={"Right Turn Assistant"}
                       type={IndustryBadge}
                       img={raa}
-                      description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."}
+                      description={"Development of a right-turn-assistant prototype for trucks. This system warns the driver of potential crashes when doing a right turn and eliminates the blind spot. Using data from multiple sensors to increase reliability."}
                       badges={[PythonBadge, JavascriptBadge, InfluxBadge, GrafanaBadge]}
+                />
+                <Card name={"IOT Protocols"}
+                      type={IndustryBadge}
+                      img={iot}
+                      description={"Development and application of different IOT protocols and evaluation for automotive use-cases."}
+                      badges={[PythonBadge, GrpcBadge, MQTTBadge, AMQPBadge]}
                 />
                 <Card name={"Workshop Booking Website"}
                       type={PrivateBadge}
                       img={handpan}
-                      description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."}
-                      badges={[JavascriptBadge, ReactBadge, FirebaseBadge, TailwindBadge]}
+                      description={`Website to book music workshops written in React. Serverless backend which handles bookings, validates confirmations and sends update mails <a href="https://handpan-atelier.web.app" style="text-decoration: underline"> (Website) </a>.`}
+                      badges={[JavascriptBadge, ReactBadge, NodeBadge, FirebaseBadge, TailwindBadge]}
                 />
                 <Card name={"Music Game Website"}
                       type={UniversityBadge}
                       img={bragi}
-                      description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."}
+                      description={`Website using the Spotify API to play games for their song to be added to the party playlist/queue <a href="https://github.com/Bragi3000/Bragi3000" style="text-decoration: underline">(Repo)</a>.`}
                       badges={[JavascriptBadge, ReactBadge, FirebaseBadge, TailwindBadge]}
                 />
                 <Card name={"Cabworld and Cablab (Thesis)"}
                       type={UniversityBadge}
                       img={cabworld}
-                      description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."}
+                      description={`Development, improvement and testing of various reinforcement learning algorithms, focusing on finding an effective algorithm for Cabworld. Cabworld is a custom environment, where multiple cabs have to learn how to bring passengers to their desired destination <a href="https://github.com/nikolim/cablab" style="text-decoration: underline">(Repo)</a>.`}
                       badges={[PythonBadge, TorchBadge]}
                 />
             </div>
