a28975869d42d48333c47eff2531a39b5ceabeb1
Nikolai Limbrunner <nikolai.limbrunner@web.de>
2022-08-23 10:51:08 +0200

    Skills

--- src/Components/skills.js
+++ src/Components/skills.js
@@ -1,11 +1,14 @@
 const Skills = function () {
+    const iconSize = 64;
     return (
         <div className="flex-col pt-9">
             <div className="text-center text-white text-4xl pb-3 font-medium">
                 Skills
             </div>
+            <div className="flex flex-wrap text-white">
         </div>
-    );
+</div>
+);
 };
 
 export default Skills;
