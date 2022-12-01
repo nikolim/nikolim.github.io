604f93f00559e051886e5b373a4122b0dadeea88
Niko Limbrunner <nikolai.limbrunner@gmail.com>
2022-12-01 14:35:14 +0100

    Fix next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  assetPrefix: isProd ? '/nikolim.github.io/' : '',
+  assetPrefix: isProd ? 'https://nikolim.github.io/' : '',
   images: {
     unoptimized: true,
   },
