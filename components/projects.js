5d75da981cab71c93848f4936eac5dd16548484c
Niko Limbrunner <nikolai.limbrunner@gmail.com>
2022-12-16 13:24:41 +0100

    Added thesis project

--- components/projects.js
+++ components/projects.js
@@ -7,6 +7,7 @@ import handpan from "../public/images/projects/handpan.png";
 import cabworld from "../public/images/projects/cabworld.png";
 import bragi from "../public/images/projects/bragi.png";
 import hulawood from "../public/images/projects/forest.png";
+import greenIT from "../public/images/projects/greenIT.jpg";
 
 const commonBadgeStyle = "text-xs font-medium mr-2 mb-2 px-2.5 py-0.5 rounded";
 
@@ -36,6 +37,10 @@ const K8sBadge = function () {
 }
 const InfluxBadge = function () {
     return (<span className={`bg-purple-100 text-purple-800 ${commonBadgeStyle}`} key={"influx"}>InfluxDB</span>);
+}
+
+const PrometheusBadge = function () {
+    return (<span className={`bg-purple-100 text-purple-800 ${commonBadgeStyle}`} key={"influx"}>Prometheus</span>);
 }
 const GrafanaBadge = function () {
     return (<span className={`bg-yellow-100 text-yellow-800 ${commonBadgeStyle}`} key={"grafana"}>Grafana</span>);
@@ -68,6 +73,13 @@ const TorchBadge = function () {
 const NodeBadge = function () {
     return (<span className={`bg-green-100 text-green-800 ${commonBadgeStyle}`} key={"nodejs"}>NodeJS</span>);
 }
+const DevOpsBadge = function () {
+    return (<span className={`bg-green-100 text-green-800 ${commonBadgeStyle}`} key={"nodejs"}>DevOps</span>);
+}
+
+const CloudBadge = function () {
+    return (<span className={`bg-blue-100 text-blue-800 ${commonBadgeStyle}`} key={"tailwind"}>Cloud</span>);
+}
 
 const Card = function ({name, type, img, description, badges, key}) {
     return (
@@ -98,10 +110,16 @@ const Projects = function () {
             </div>
             <div
                 className="p-4 grid grid-cols-1 sm:grid-cols-1 sm:p-4 md:grid-cols-2 md:p-6 lg:grid-cols-3 lg:p-8 xl:grid-cols-3 xl:p-10 gap-8 m-auto max-w-6xl">
+                <Card name={"Master Thesis"}
+                      type={IndustryBadge}
+                      img={greenIT}
+                      description={`Dynamic macro to micro scale calculation of energy consumption metrics in CI/CD pipelines. Creation of transparent and fine-granular energy measurements for pipelines and derivation of possible improvements.`}
+                      badges={[DevOpsBadge, CloudBadge, PrometheusBadge, GrafanaBadge, PythonBadge, GolangBadge]}
+                />
                 <Card name={"CLM Lane Management"}
                       type={IndustryBadge}
                       img={clm}
-                      description={`Development of distributed backend services and onboard software to enable cooperative maneuvers like lane merging of connected vehicles. Levering 5G and Mobile-Edge-Computing to reach low latency <a href="https://www.youtube.com/watch?v=daxptHOXTI4" style="text-decoration: underline">(Promo video).</a>`}
+                      description={`Development of distributed backend services and onboard software to enable cooperative maneuvers like lane merging of connected vehicles. Levering 5G and Mobile-Edge-Computing to reach low latency (<a href="https://www.youtube.com/watch?v=daxptHOXTI4" style="text-decoration: underline">Promo video</a>).`}
                       badges={[PythonBadge, JavascriptBadge, GolangBadge, AMQPBadge, K8sBadge, InfluxBadge, GrafanaBadge]}
                 />
                 <Card name={"Right Turn Assistant"}
@@ -119,25 +137,25 @@ const Projects = function () {
                 <Card name={"Hulawood"}
                       type={PrivateBadge}
                       img={hulawood}
-                      description={`Hulawood creates a new way to invest sustainably in forest as an asset class. <a href="https://www.hulawood.com/" style="text-decoration: underline"> (Website) </a>.`}
+                      description={`Hulawood creates a new way to invest sustainably in forest as an asset class. (<a href="https://www.hulawood.com/" style="text-decoration: underline">Website</a>).`}
                       badges={[JavascriptBadge, NextBadge, FirebaseBadge, TailwindBadge]}
                 />
                 <Card name={"Workshop Booking Website"}
                       type={PrivateBadge}
                       img={handpan}
-                      description={`Website to book music workshops written in React. Serverless backend which handles bookings, validates confirmations and sends update mails <a href="https://handpan-atelier.web.app" style="text-decoration: underline"> (Website) </a>.`}
+                      description={`Website to book music workshops written in React. Serverless backend which handles bookings, validates confirmations and sends update mails (<a href="https://handpan-atelier.web.app" style="text-decoration: underline">Website</a>).`}
                       badges={[JavascriptBadge, ReactBadge, NodeBadge, FirebaseBadge, TailwindBadge]}
                 />
                 <Card name={"Music Game Website"}
                       type={UniversityBadge}
                       img={bragi}
-                      description={`Website using the Spotify API to play games for their song to be added to the party playlist/queue <a href="https://github.com/Bragi3000/Bragi3000" style="text-decoration: underline">(Repo)</a>.`}
+                      description={`Website using the Spotify API to play games for their song to be added to the party playlist/queue (<a href="https://github.com/Bragi3000/Bragi3000" style="text-decoration: underline">Repo</a>).`}
                       badges={[JavascriptBadge, ReactBadge, FirebaseBadge, TailwindBadge]}
                 />
                 <Card name={"Cabworld and Cablab (Thesis)"}
                       type={UniversityBadge}
                       img={cabworld}
-                      description={`Development, improvement and testing of various reinforcement learning algorithms, focusing on finding an effective algorithm for Cabworld. Cabworld is a custom environment, where multiple cabs have to learn how to bring passengers to their desired destination <a href="https://github.com/nikolim/cablab" style="text-decoration: underline">(Repo)</a>.`}
+                      description={`Development, improvement and testing of various reinforcement learning algorithms, focusing on finding an effective algorithm for Cabworld. Cabworld is a custom environment, where multiple cabs have to learn how to bring passengers to their desired destination (<a href="https://github.com/nikolim/cablab" style="text-decoration: underline">Repo</a>).`}
                       badges={[PythonBadge, TorchBadge]}
                 />
             </div>
