3fdfcf34a1de9730f65c21f060c2c9a13f8ddcd2
nikolim <nikolim@users.noreply.github.com>
2022-12-16 12:30:22 +0000

    deploy: 5d75da981cab71c93848f4936eac5dd16548484c

---
+++ _next/static/O1Nvc5DI548I5yw9Lz1e3/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/753-39d573cde7c5f724.js","static/chunks/pages/index-170c9109d6875094.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
